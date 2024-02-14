a3acebe1528372e9f65bc9070a5176eddf972da5
Ming Bian <mynameisking67@gmail.com>
2024-02-14 09:55:20 -0500

    initial commit

---
+++ Assets/ReactiveTarget.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReactiveTarget : MonoBehaviour {
+
+	public void ReactToHit() {
+		WanderingAI behavior = GetComponent<WanderingAI>();
+		if (behavior != null) {
+			behavior.SetAlive(false);
+		}
+		StartCoroutine(Die());
+	}
+
+	private IEnumerator Die() {
+		this.transform.Rotate(-75, 0, 0);	
+		yield return new WaitForSeconds(1.5f);
+		Destroy(this.gameObject);
+	}
+
+	private IEnumerator RotateoverTime(){
+		float rotationSpeed = 90f;
+		float rotationAmount = 0f;
+
+		while(rotationAmount < 90f){
+
+			float rotationThisFrame = rotationSpeed*Time.deltaTime;
+			transform.Rotate(Vector3.up, rotationThisFrame);
+			rotationAmount += rotationThisFrame;
+			yield return null;
+			
+		}
+	}
+}
